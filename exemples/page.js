25fc2e105fbeec3d38893abbac9e61f24420c79e
Julien Decaudin <julien.decaudin@radiofrance.com>
2014-10-09 11:48:55 +0200

    Add better exemple (with preprocess, process, callback)

--- exemples/page.js
+++ exemples/page.js
@@ -10,6 +10,41 @@ module.exports = {
     },
   },
   methods : [
-    {verb : 'get'},
+    {
+      verb: 'get',
+      preprocess: [pagePreprocessOne, pagePreprocessTwo],
+      process: pageProcess,
+      callback: [pageCallbackOne]
+    },
+    {verb: 'post'},
   ]
 };
+
+function pagePreprocessOne(req, res, next) {
+  console.log('Hello ');
+  req.helloWorld = 'World';
+
+  next();
+}
+
+function pagePreprocessTwo(req, res, next) {
+  console.log(req.helloWorld + ' :)');
+
+  next();
+}
+
+function pageProcess(req, res, app, method, callback) {
+  // Do anything here
+  var results = ['array', 'of', 'results'];
+
+  //!\ You must return something to the client here !
+  res.send(results);
+
+  callback(req, res, app, method, results);
+}
+
+function pageCallbackOne(req, res, app, method, results, next) {
+  console.log("Super callback !", results);
+
+  next();
+}
